ee1db4278cc33944b3c926809e0e025a75af1e9e
Valeria <valethebesthola469@gmail.com>
2023-08-23 10:49:11 +0200

    Correction in setting

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
         self.bullet_speed = 6.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet.color = (60, 60, 60)
+        self.bullet_color = (60, 60, 60)
         
 
         
