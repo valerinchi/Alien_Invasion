8237346d82dc68883048df85ad30e1328c95c538
Valeria <valethebesthola469@gmail.com>
2023-08-24 08:47:09 +0200

    Limiting the number of bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,7 +32,6 @@ class AlienInvasion:
             for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
-            print(len(self.bullets))
 
             self._update_screen()
             self.clock.tick(60)
@@ -67,8 +66,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bulles group."""
-        new_bullet = Bullet(self) 
-        self.bullets.add(new_bullet)  
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self) 
+            self.bullets.add(new_bullet)  
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
